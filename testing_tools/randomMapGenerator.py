25739f96f9b81abef82f2c7528bd7b6c024b677a
Matthew Booker <mattbooker97@gmail.com>
2020-05-21 15:00:24 +0800

    Added non_rectilinear feature

--- testing_tools/randomMapGenerator.py
+++ testing_tools/randomMapGenerator.py
@@ -1,13 +1,16 @@
-from os.path import dirname, join, abspath
+import math
+import random
+import time
+from os.path import abspath, dirname, join
+
+import numpy as np
 from pyrep import PyRep
+from pyrep.const import PrimitiveShape
 from pyrep.objects.shape import Shape
 from pyrep.objects.vision_sensor import VisionSensor
-from pyrep.const import PrimitiveShape
-import numpy as np
-import time
-import random
 
-def generate_random_map(total_objects, only_rectilinear=True):
+
+def generate_random_map(total_objects, non_rectilinear=False):
 
 	# Set the random seed in the main module
 
@@ -22,14 +25,24 @@ def generate_random_map(total_objects, only_rectilinear=True):
 	for i in range(total_objects):
 		
 		while True:
-			#TODO: Set orientation if only_rectilinear is false
 
 			random_x_size = random.uniform(0.1*size_x, 0.5*size_x)
 			random_y_size = random.uniform(0.1*size_y, 0.5*size_y)
 
+
+
 			random_x_pos = random.uniform(random_x_size/2, size_x - random_x_size/2) - 0.5
 			random_y_pos = random.uniform(random_y_size/2, size_y - random_y_size/2) - 0.5
-			new_shape = Shape.create(type=PrimitiveShape.CUBOID, size=[random_x_size,random_y_size,.05], position=[random_x_pos,random_y_pos,0.025], static=True, renderable=True)
+			
+			yaw = random.uniform(0, math.pi) if non_rectilinear else 0
+
+			new_shape = Shape.create(type=PrimitiveShape.CUBOID, 
+															 size=[random_x_size,random_y_size,.05], 
+															 position=[random_x_pos,random_y_pos,0.025], 
+															 orientation=[0,0,yaw],
+															 static=True, 
+															 renderable=True)
+
 			new_shape.set_collidable(True)
 
 			if new_shape.check_collision() == False:
